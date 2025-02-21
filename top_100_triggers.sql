6dd3634a2d25d9f2a83582b722385335459b9f58
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2025-02-21 10:03:58 -0300

    Update top_100_triggers.sql

--- top_100_triggers.sql
+++ top_100_triggers.sql
@@ -26,3 +26,36 @@ GROUP BY
 ORDER BY 
     count DESC 
 LIMIT 100;
+
+
+
+--- Ou então, para usar no grafana com variaveis de data:
+
+
+SELECT 
+    h.name, 
+    t.description, 
+    COUNT(e.eventid) AS 'count' 
+FROM 
+    hosts h 
+INNER JOIN 
+    items i ON h.hostid = i.hostid 
+INNER JOIN 
+    functions f ON i.itemid = f.itemid 
+INNER JOIN 
+    triggers t ON f.triggerid = t.triggerid 
+INNER JOIN 
+    events e ON t.triggerid = e.objectid 
+    AND e.object = 0 
+    AND e.source = 0 
+    AND e.value = 1 
+    AND FROM_UNIXTIME(e.clock) BETWEEN $__timeFrom() AND $__timeTo() -- Filtro de tempo dinâmico do Grafana
+WHERE 
+    h.status = 0 
+    AND i.status = 0 
+    AND t.status = 0 
+GROUP BY 
+    h.name, 
+    t.description 
+ORDER BY 
+    count DESC 
